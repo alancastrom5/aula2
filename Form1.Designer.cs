0b5f8defcb390252f9f8b50b47a1433bbd3d2614
alancastrom5 <79289644+alancastrom5@users.noreply.github.com>
2023-03-09 21:00:25 -0300

    up

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,22 +29,36 @@
         private void InitializeComponent()
         {
             label1 = new Label();
+            button1 = new Button();
             SuspendLayout();
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(18, 21);
+            label1.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(317, 49);
             label1.Name = "label1";
-            label1.Size = new Size(40, 15);
+            label1.Size = new Size(144, 32);
             label1.TabIndex = 0;
-            label1.Text = "Aula 2";
+            label1.Text = "Bem vindo!";
+            // 
+            // button1
+            // 
+            button1.BackColor = SystemColors.ActiveCaption;
+            button1.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.Location = new Point(317, 139);
+            button1.Name = "button1";
+            button1.Size = new Size(144, 44);
+            button1.TabIndex = 1;
+            button1.Text = "Exibir frase";
+            button1.UseVisualStyleBackColor = false;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(button1);
             Controls.Add(label1);
             Name = "Form1";
             Text = "Form1";
@@ -55,5 +69,6 @@
         #endregion
 
         private Label label1;
+        private Button button1;
     }
 }
